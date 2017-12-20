e8fa2b6169ce30e0f70624314ed82d13f8510f99
edschultz <eschultz@showingtime.com>
2017-12-20 10:43:01 -0600

    Get search working from web and controller.

--- MyFirstWeb/ClientApp/app/components/search/search.component.ts
+++ MyFirstWeb/ClientApp/app/components/search/search.component.ts
@@ -9,16 +9,14 @@ export class SearchComponent {
     public results: SearchData;
     public count: string;
 
-    constructor(http: Http) {
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/Search/Results').subscribe(result => {
+            this.results = result.json();
+        }, error => console.error(error));
         http.get('http://localhost:9200/infoserv_27/_count').subscribe(result => {
             this.count = result.json();
         }, error => console.error(error));
     }
-    //constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-    //    http.get(baseUrl + 'api/Search/Results').subscribe(result => {
-    //        this.results = result.json();
-    //    }, error => console.error(error));
-    //}
 }
 
 interface SearchData {
